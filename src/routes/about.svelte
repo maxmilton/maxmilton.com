b51fa4a501d08b10df7030230771f3da5ebf6312
Max Milton <max@wearegenki.com>
2020-01-17 06:55:15 +1100

    Reorder and clean up about me

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,26 +6,13 @@
 <div class="con">
   <h1>About Max Milton</h1>
 
-  <blockquote>
-    <p>&ldquo;Productivity is never an accident. It is always the result of a commitment to excellence, intelligent planning, and focused effort.&rdquo;</p>
-    <footer class="blockquote-footer">Paul J. Meyer</footer>
-  </blockquote>
-
   <p>Yo ✌</p>
   <p>I'm Max and this is my personal blog. Welcome.</p>
 
-  <p>// // // // // // // // // // // //</p>
-
+  <!--
   <p>I'm Max. I'm a tech consultant currently based in Sydney, Australia. I'm always up for new experiences, love learning new things, my world is full of adventure and crazy fun times!</p>
   <p>I specialise in Ecommerce, DevOps, Linux, and Cloud Infrastructure. I typically work with enterprise and government on large contract projects but I love working with start-ups on occasion too. To hire me for a project or consulting <a href="about#contact-max">contact me</a>.</p>
   <p>I also run a number of ecommerce properties via my company <a href="https://wearegenki.com">We Are Genki</a>. These sites are a great example of the quality of my work.</p>
-
-  <h2>Find me on GitHub</h2>
-
-  <ul>
-    <li><a href="https://github.com/MaxMilton" target="_blank">github.com/MaxMilton</a></li>
-    <li><a href="https://github.com/WeAreGenki" target="_blank">github.com/WeAreGenki</a></li>
-  </ul>
 
   <h2>Did you know...?</h2>
 
@@ -36,10 +23,23 @@
     <li>When I move to a new city I make friends by going to tech and business meetups.</li>
     <li>I eat a <a href="blog/intro-to-the-keto-diet-and-14-day-keto-meal-plan">diet consisting mostly of fat</a>.</li>
   </ul>
+  -->
+
+  <blockquote>
+    <p>&ldquo;Productivity is never an accident. It is always the result of a commitment to excellence, intelligent planning, and focused effort.&rdquo;</p>
+    <footer class="blockquote-footer">Paul J. Meyer</footer>
+  </blockquote>
+
+  <h2>Find me on GitHub</h2>
+
+  <ul>
+    <li><a href="https://github.com/MaxMilton" target="_blank">github.com/MaxMilton</a></li>
+    <li><a href="https://github.com/WeAreGenki" target="_blank">github.com/WeAreGenki</a></li>
+  </ul>
 
   <hr>
 
-  <h2 id="contact-max">Contact Max</h2>
+  <h2 id="contact-max">Contact Me</h2>
 
   <form action="https://docs.google.com/a/wearegenki.com/forms/d/1viSY2eAr1kZ9cIf7vTwQpWaB4Zoix7xPnsbhTUcZrTw/formResponse" method="POST" target="_blank">
     <div class="form-group">
@@ -75,11 +75,13 @@
     <button type="submit" class="button">Send your message</button>
   </form>
 
-  <p>Alternatively, send an email to <a href="mailto:max@wearegenki.com">max@wearegenki.com</a>.</p>
+  <p>Alternatively send an email to <a href="mailto:max@wearegenki.com">max@wearegenki.com</a>.</p>
 
   <hr>
 
   <h2>Copyright</h2>
 
-  <p>All blog posts are licensed under the Creative Commons Attribution 4.0 International (<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>) licence. Want to share a post? You're welcome to, however, please link back to the original article.</p>
+  <p>Unless otherwise noted, blog posts are licensed Creative Commons Attribution 4.0 (<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="nofollow">CC BY 4.0</a>). Want to share a post? You're welcome to but please link back to the original article.</p>
+
+  <p>This website is an <a href="https://github.com/MaxMilton/MaxMilton.com/blob/master/LICENCE" target="_blank" rel="nofollow">MIT licensed</a> open source project. View the source code: <a href="https://github.com/MaxMilton/MaxMilton.com" target="_blank" rel="nofollow">github.com/MaxMilton/MaxMilton.com</a>.</p>
 </div>
