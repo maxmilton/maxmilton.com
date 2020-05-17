b0fd546ec4ee23bd903ef620c35c5a798f2d5592
Max Milton <max@wearegenki.com>
2020-05-17 12:03:27 +1000

    Fix lint error

--- src/routes/blog/rss.xml.ts
+++ src/routes/blog/rss.xml.ts
@@ -29,9 +29,9 @@ const rss = (posts: PostItem[]): string =>
     .map(
       (post) => `
     <item>
-      <title>${post.metadata.title}</title>
+      <title>${post.metadata.title || ''}</title>
       <link>https://svelte.dev/blog/${post.slug}</link>
-      <description>${post.metadata.description}</description>
+      <description>${post.metadata.description || ''}</description>
       <pubDate>${formatPubdate(post.metadata.pubdate)}</pubDate>
     </item>
   `,
