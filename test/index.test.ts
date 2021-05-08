3032fba0448eb96f6689c8de99502921dd1c1939
Max Milton <max@wearegenki.com>
2021-05-09 09:07:55 +1000

    Add test to verify blog content is generated

---
+++ test/index.test.ts
@@ -0,0 +1,22 @@
+describe('app static export', () => {
+  it('generates blog.json correctly', () => {
+    expect.assertions(3);
+    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, global-require
+    const json = require('../__sapper__/export/blog.json');
+    expect(Array.isArray(json)).toBe(true);
+    expect(json.length).toBeGreaterThan(0);
+    expect(json[0]).toMatchObject({
+      metadata: {
+        title: expect.stringMatching(/^.+$/),
+        description: expect.stringMatching(/^.+$/),
+        pubdate: expect.stringMatching(/^.+$/),
+        date: expect.stringMatching(/^.+$/),
+      },
+      slug: expect.stringMatching(/^.+$/),
+    });
+  });
+});
+
+// keep TS happy
+// eslint-disable-next-line jest/no-export
+export {};
