6b56a8eb666c867e723d712ff769d65c296ed435
Max Milton <max@wearegenki.com>
2021-05-09 09:10:09 +1000

    Fix CI lint error

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 describe('app static export', () => {
   it('generates blog.json correctly', () => {
     expect.assertions(3);
-    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, global-require
+    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, global-require, import/no-unresolved
     const json = require('../__sapper__/export/blog.json');
     expect(Array.isArray(json)).toBe(true);
     expect(json.length).toBeGreaterThan(0);
